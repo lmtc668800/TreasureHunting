b872bf0703d611cbd5544478b3f6a4c771185743
lmtc668800 <lmtc668800@gmail.com>
2016-07-27 12:56:00 +0900

    modified navigation bar

--- app/src/main/java/com/examplexample/treasurehunting/MainActivity.java
+++ app/src/main/java/com/examplexample/treasurehunting/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Synchronize current location", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -80,17 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.map) {
+            // Handle the map action
+        } else if (id == R.id.step_counter) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.information) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.setting) {
 
         }
 
