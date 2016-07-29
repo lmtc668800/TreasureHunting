60c1d2222ff5df1fb910ea5d52303cd2b202335b
lmtc668800 <lmtc668800@gmail.com>
2016-07-30 00:01:15 +0900

    add simple step counter

--- app/src/main/java/com/examplexample/treasurehunting/BlankFragment.java
+++ app/src/main/java/com/examplexample/treasurehunting/BlankFragment.java
@@ -1,29 +1,100 @@
 package com.examplexample.treasurehunting;
 
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BlankFragment extends Fragment {
+public class BlankFragment extends Fragment implements SensorEventListener {
 
+    private SensorManager mSensorManager;
+    private Sensor mStepDetectorSensor;
+    private Sensor mStepCounterSensor;
 
     public BlankFragment() {
         // Required empty public constructor
+
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        //センサーマネージャを取得
+
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+
+        mSensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+
+        //センサマネージャから TYPE_STEP_DETECTOR についての情報を取得する
+        mStepDetectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+
+        //センサマネージャから TYPE_STEP_COUNTER についての情報を取得する
+        mStepCounterSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView stepcount = (TextView) view.findViewById(R.id.stepcount);
+        stepcount.setText("Waiting for start");
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // accuracy に変更があった時の処理
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        Sensor sensor = event.sensor;
+        float[] values = event.values;
+        long timestamp = event.timestamp;
+        //TYPE_STEP_COUNTER
+        if (sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            // sensor からの値を取得するなどの処理を行う
+            TextView stepcount=(TextView) getView().findViewById(R.id.stepcount);
+            TextView stepleft=(TextView) getView().findViewById(R.id.stepleft);
+            stepcount.setText((int)values[0]+ " steps");
+            int leftstep = 6000 -(int)values[0];
+            stepleft.setText(leftstep + "steps left...");
+//            Log.d("type_step_counter", String.valueOf(values[0]));
+        }
+
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mSensorManager.registerListener (this,
+                mStepCounterSensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+
+        mSensorManager.registerListener(this,
+                mStepDetectorSensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this, mStepCounterSensor);
+        mSensorManager.unregisterListener(this,mStepDetectorSensor);
     }
 
 }
